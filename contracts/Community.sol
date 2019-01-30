2144f6f4c2a7c26f9202c8542b116683df547139
Haythem Sellami <sellamihaythem96@gmail.com>
2019-01-30 15:39:58 +0100

    create space

--- contracts/Community.sol
+++ contracts/Community.sol
@@ -6,17 +6,43 @@ import "@aragon/os/contracts/lib/math/SafeMath.sol";
 
 contract Community is AragonApp {
 
+    //space strcuture
     struct Space {
         bytes32 id;
-        bytes32 coin;
         string name;
         string description;
+        address owner;
         address[] members;
-
     }
+
+    //number of spaces
+    uint256 spacesCount;
+
+    mapping(address => uint256[]) public ownerSpaces;
+
+    //list of all spaces
+    Space[] communities;
 
     function initialize() public onlyInit {
         initialized();
+    }
+
+    /**
+     * @notice function to be called by member to create new space
+     * @param _id bytes32 keccak256 of the space name
+     * @param _name string space name
+     * @param _desc string space description
+     * @param _members Array of members addresses
+     */
+    function _createSpace(bytes32 _id, string _name, string _desc,address _owner, address[] _members) internal isInitialized {
+        Space memory community = Space(_id, _name, _desc, _owner, _members);
+        //and new space and update total spaces number
+        spacesCount = communities.push(community);
+        //get owner spaces
+        uint256[] storage relatedSpaces = ownerSpaces[_owner];
+        //add new space
+        relatedSpaces.push(spacesCount);
+        ownerSpaces[_owner] = relatedSpaces;
     }
 
 }
--- contracts/Member.sol
+++ contracts/Member.sol
@@ -4,8 +4,10 @@ import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/lib/math/SafeMath.sol";
 import "@aragon/os/contracts/lib/token/ERC20.sol";
 
+import "./Community.sol";
 
-contract Member is AragonApp {
+
+contract Member is AragonApp, Community {
 
     bytes32 public constant ISSUE_ROLE = keccak256("ISSUE_ROLE");
     bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
@@ -24,5 +26,32 @@ contract Member is AragonApp {
 
     function initialize() public onlyInit {
         initialized();
+    }
+
+    /**
+     * @notice function to create new space
+     * @param _name space name
+     * @param _desc space description
+     * @param _members list of members addresses
+     */
+    function createSpace(string _name, string _desc, address[] _members) public {
+        require(VerifyMembers(_members), "invalid member address");
+        
+        bytes32 spaceId = keccak256(abi.encodePacked(_name));
+        _createSpace(spaceId, _name, _desc, msg.sender, _members);
+    }
+
+    /**
+     * @notice function to verify addresses
+     * @param _members list of addresses
+     * @return true if all addresses are valid, otherwise return false
+     */
+    function VerifyMembers(address[] _members) internal pure returns(bool) {
+        for(uint i = 0; i < _members.length; i++) {
+            if(_members[i] == address(0)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
