e90782ba2798890e6e8063b7fb8a5fce1785b5ab
Haythem Sellami <sellamihaythem96@gmail.com>
2019-02-21 14:54:34 +0100

    unit tests

--- Community/space/test/community.js
+++ Community/space/test/community.js
@@ -1,8 +1,8 @@
 const Space = artifacts.require("Community.sol");
-const DAOFactory = artifacts.require('@aragon/os/contracts/factory/DAOFactory');
-const EVMScriptRegistryFactory = artifacts.require('@aragon/os/contracts/factory/EVMScriptRegistryFactory');
+
 const ACL = artifacts.require('@aragon/os/contracts/acl/ACL');
 const Kernel = artifacts.require('@aragon/os/contracts/kernel/Kernel');
+const DAOFactory = artifacts.require('@aragon/os/contracts/factory/DAOFactory');
 
 const namehash = require('eth-ens-namehash').hash;
 const keccak256 = require('keccak256');
@@ -13,53 +13,151 @@ const timeTravel = require('@aragon/test-helpers/timeTravel')(web3);
 
 const getContract = name => artifacts.require(name);
 
-contract('Space App', (accounts) => {
-  let spaceBase, daoFact, space;
+const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
 
-  let APP_MANAGER_ROLE;
-  let MANAGER_ROLE, MINT_ROLE;
+contract('Space app', (accounts) => {
+  let kernelBase, aclBase, daoFactory, dao, acl, space;
 
   const root = accounts[0];
-
-  let coinsenceDeployer;
-  let member1;
-  let member2;
-  let member3;
-  let member4;
-  let member5;
+  const member1 = accounts[1];
 
   before(async() => {
+    //init kernel base
+    kernelBase = await Kernel.new(true) // immediately petrify
+    //init acl base
+    aclBase = await ACL.new()
+    //init new dao factory
+    daoFactory = await DAOFactory.new(
+      kernelBase.address,
+      aclBase.address,
+      ZERO_ADDR
+    );
 
-    const kernelBase = await getContract('Kernel').new(true);
-    const aclBase = await getContract('ACL').new();
-    const regFact = await EVMScriptRegistryFactory.new();
-    daoFact = await DAOFactory.new(kernelBase.address, aclBase.address, regFact.address);    
+    //init new dao from dao factory
+    const r = await daoFactory.newDAO(root)
+    dao = Kernel.at(
+      r.logs.filter(l => l.event == 'DeployDAO')[0].args.dao
+    );
+    //get DAO acl
+    acl = ACL.at(await dao.acl());
+
+    //create dao mamnager permission for space owner
+    await acl.createPermission(
+      root,
+      dao.address,
+      await dao.APP_MANAGER_ROLE(),
+      root,
+      { from: root }
+    );
+
+    //get new app instance from DAO
+    const receipt = await dao.newAppInstance(
+      '0x1234',
+      (await Space.new()).address,
+      0x0,
+      false,
+      { from: root }
+    )
+    space = Space.at(
+      receipt.logs.filter(l => l.event == 'NewAppProxy')[0].args.proxy
+    )
+
+    //init space (app)
+    await space.initialize("coinsence", []);
+
+    //create space manager permission for space owner
+    await acl.createPermission(
+      root,
+      space.address,
+      await space.MANAGER_ROLE(),
+      root,
+      { from: root }
+    )
+
+    //create token issuer permission for space owner
+    await acl.createPermission(
+      root,
+      space.address,
+      await space.ISSUE_TOKEN_ROLE(),
+      root,
+      { from: root }
+    )
+  
+  });
+  
+  describe("DAO", async() => {
+
+    it('check initialized DAO', async() => {
+      assert.notEqual(dao, undefined);
+      console.log(dao.address);
+    });
+
+    it('check owner is app manager', async() => {
+      let daoManagerPermission = await acl.hasPermission(root, dao.address, await dao.APP_MANAGER_ROLE());
+      assert.equal(daoManagerPermission, true);
+    });
+
   });
 
-  describe("common tests", () => {
-    
-    beforeEach(async () => {
-      spaceBase = await Space.new(daoFact.address, namehash('coinsence-aragon.aragonpm.eth'), "coinsence", []);
+  describe("Owner default space permissions", async() => {
+
+    it('check owner is space manager', async() => {
+      let spaceManagerPermission = await acl.hasPermission(root, space.address, await space.MANAGER_ROLE());
+      assert.equal(spaceManagerPermission, true);
     });
 
-    it('fails on reinitialization', async () => {
+    it('check owner is token issuer', async() => {
+      let tokenIssuerPermission = await acl.hasPermission(root, space.address, await space.ISSUE_TOKEN_ROLE());
+      assert.equal(tokenIssuerPermission, true);
+    });
+  });
+
+  describe("Space members management", async () => {
+
+    it("should revert when adding a member from an address that does not have manager permission", async() => {
       return assertRevert(async () => {
-        await spaceBase.initialize();
+        await space.addMember(accounts[2], { from: member1 })
+        'address does not have permission to add member'
       })
-    })
-
-    it('checks it is forwarder', async () => {
-        assert.isTrue(await spaceBase.isForwarder())
     });
 
-    it('create new instance with role assigned to owner', async() => {
-      let roles = [
-        keccak256("MANAGER_ROLE"),
-        keccak256("MINT_ROLE") 
-      ]
+    it("should revert when adding a member address same as `msg.sender`", async() => {
+      return assertRevert(async () => {
+        await space.addMember(root, { from: root })
+        'member address should be different from msg.sender'
+      })
+    });
 
-      //space = await spaceBase.newInstance(namehash('coinsence-aragon.aragonpm.eth'), roles, accounts[0], 0x0);
-      await space.initialize();
+    it("should revert when adding a member with address(0)", async() => {
+      return assertRevert(async () => {
+        await space.addMember(ZERO_ADDR, { from: root })
+        'member address should be different from msg.sender'
+      })
+    });
+
+    it("add new member", async() => {
+      await space.addMember(member1, { from: root });
+      assert.equal(await space.isMember(member1), true);
+    });
+  });
+
+  describe("Space members permissions management", async() => {
+    
+    //TODO: permissions should be directly assigned in the solidity function
+    before(async() => {
+      await acl.grantPermission(
+        member1,
+        space.address,
+        await space.MANAGER_ROLE(),
+        { from: root }
+      )
+    });
+
+    it("Grant space manager permission to new member from space owner", async() => {
+      let memberSpaceManagerPermission = await acl.hasPermission(member1, space.address, await space.MANAGER_ROLE());
+      assert.equal(memberSpaceManagerPermission, true);
+      let ownerSpaceManagerPermission = await acl.hasPermission(root, space.address, await space.MANAGER_ROLE());
+      assert.equal(ownerSpaceManagerPermission, true);
     });
 
   });
