b045f6ebc6803568cbf66283328139fc976e9f86
Haythem Sellami <sellamihaythem96@gmail.com>
2019-03-07 17:04:04 +0100

    issueToken & mintToken

--- apps/coin/contracts/Coin.sol
+++ apps/coin/contracts/Coin.sol
@@ -12,19 +12,40 @@ contract Coin is EtherTokenConstant, StandardCoin, AragonApp {
 
     bytes32 public constant ISSUE_ROLE = keccak256("ISSUE_ROLE");
     bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
-    bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
-    bytes32 public constant ASSIGN_ROLE = keccak256("ASSIGN_ROLE");
+    //bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+    //bytes32 public constant ASSIGN_ROLE = keccak256("ASSIGN_ROLE");
 
     string private _name;
     string private _symbol;
     uint8 private _decimals;
 
-    function initialize(string memory name, string memory symbol, uint8 decimals) public onlyInit {
+    function initialize() public onlyInit {
+
+        initialized();
+    }
+
+    function name() public view isInitialized returns(string) {
+        return _name;
+    }
+
+    function symbol() public view isInitialized returns(string) {
+        return _symbol;
+    }
+
+    function decimal() public view isInitialized returns(uint8) {
+        return _decimals;
+    }
+
+    function issueToken(string memory name, string memory symbol, uint8 decimals) public isInitialized auth(ISSUE_ROLE) {
         _name = name;
         _symbol = symbol;
         _decimals = decimals;
+    }
 
-        initialized();
+    function mintToken(uint256 amount) public isInitialized auth(MINT_ROLE) {
+        require(amount > 0, "amount equal to zero");
+
+        _mint(msg.sender, amount);
     }
 
 }
